d776b66a5c0340972ec2dcad4ed8340b7bf43bdf
yury-tikhomirov <yury.tikhomirov@supermetrics.com>
2022-01-12 17:30:01 +0200

    Create a dbt project

---
+++ models/q2_1.sql
@@ -0,0 +1,18 @@
+{{ config(materialized='table') }}
+
+WITH lineorder_flat AS (
+    SELECT * FROM {{ source('dbgen','lineorder_wide')}}
+)
+
+SELECT
+    sum(LO_REVENUE),
+    toYear(LO_ORDERDATE) AS year,
+    P_BRAND
+FROM lineorder_flat
+WHERE P_CATEGORY = 'MFGR#12' AND S_REGION = 'AMERICA'
+GROUP BY
+    year,
+    P_BRAND
+ORDER BY
+    year,
+    P_BRAND
